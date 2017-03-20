d321d6211e0236794b596ef96a9c4f2d49a1380d
jahnava <jahnavalmt@gmail.com>
2017-03-20 00:38:50 -0400

    Added show/hide functions and showAll/clearAll functions.  Changed css to have white overlay with silver grid.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var totalBoxes = 500;
+var totalBoxes = 524;
 var screenWidth = window.innerWidth;
 var screenHeight = window.innerHeight;
 
@@ -8,13 +8,45 @@ console.log(screenHeight);
 //width (px) of one box = screenWidth/columns
 //height (px) of one box = screenHeight/rows
 
-for(var p = 0; p < 501; p++){
+for(var p = 0; p < 525; p++){
   var new_col_div = document.createElement("div");
   new_col_div.className = "col-xs-1";
   document.body.appendChild(new_col_div);
   new_col_div.id = `${p}` //tag each box with the id row,column
-
 }
 
 
 //upon page reload, choose x number of random boxes to fill in
+
+
+
+
+
+
+
+function clearAll(){
+  for (i=0;i<=totalBoxes;i++){
+  var thisBoxId = i;
+    hide(thisBoxId);
+  }
+}
+
+function showAll(){
+  for (i=0;i<=totalBoxes;i++){
+  var thisBoxId = i;
+    show(thisBoxId);
+  }
+}
+
+// hiding the div object makes the picture behind it visible
+function show(objectId){
+  var obj = document.getElementById(objectId);
+  obj.setAttribute('style','visibility:hidden');
+}
+
+
+// showing the div blocks the image behind it
+function hide(objectId){
+  var obj = document.getElementById(objectId);
+  obj.setAttribute('style','visibility:visible');
+}
